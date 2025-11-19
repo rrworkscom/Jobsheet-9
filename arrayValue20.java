3297fa4d30d7faa460cc3701d459d5753bd18b6e
rrworkscom <rrworks010@gmail.com>
2025-11-19 20:41:16 +0700

    arrayValue20.java

---
+++ arrayValue20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class arrayValue20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] finalScore = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalScore[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final score " + i + " is " + finalScore[i]);
+        }
+    }
+}
