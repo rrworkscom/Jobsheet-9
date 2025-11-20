82c2ba601d6c48647a2dd2541258616079a41d29
rrworkscom <rrworks010@gmail.com>
2025-11-20 17:10:57 +0700

    Extended code

--- linearSearch20.java
+++ linearSearch20.java
@@ -1,15 +1,37 @@
+import java.util.Scanner;
+
 public class linearSearch20 {
     public static void main(String[] args) {
-        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int result = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of array elements: ");
+        int n = input.nextInt();
+
+        int[] arrayInt = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the array element " + i + ": ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Enter the key you want to search for: ");
+        int key = input.nextInt();
+
+        int result = -1;
+
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 result = i;
                 break;
             }
         }
-        System.out.println("The key in the array is located at index position " + result);
 
+        if (result != -1) {
+            System.out.println("The key in the array is located at index position " + result);
+        } else {
+            System.out.println("The key is not found in the array");
+        }
+
+        input.close();
     }
 }
