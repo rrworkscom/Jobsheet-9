36f1beee5479961f642c7b528e7751541db21ac2
rrworkscom <rrworks010@gmail.com>
2025-11-20 16:23:17 +0700

    For Loop added

--- arrayNumbers20.java
+++ arrayNumbers20.java
@@ -1,16 +1,10 @@
 public class arrayNumbers20 {
     public static void main(String[] args) {
 
-    int[] num = new int[4];
-    num[0] = 5;
-    num[1] = 12;
-    num[2] = 7;
-    num[3] = 20;
+        int[] num = {5, 12, 7, 20};
 
-    System.out.println(num[0]);
-    System.out.println(num[1]);
-    System.out.println(num[2]);
-    System.out.println(num[3]);
-}
-    
+       for (int i = 0; i < 4; i++) {
+        System.out.println(num[i]);
+       }
+    }
 }
