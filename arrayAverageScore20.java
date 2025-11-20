1b520f3c590f5356e96e0f31851da8369775383d
rrworkscom <rrworks010@gmail.com>
2025-11-20 17:03:43 +0700

    Passed Failed Average Score added

--- arrayAverageScore20.java
+++ arrayAverageScore20.java
@@ -4,19 +4,36 @@ public class arrayAverageScore20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] score = new int[10];
-        double total = 0;
-        double average;
+        System.out.print("Enter the number of students: ");
+        int n = sc.nextInt();
 
-        for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter student score " + (i + 1) + ": ");
-            score[i] = sc.nextInt();
-        }
-        for (int i = 0; i < score.length; i++) {
-            total += score[i];
+        int[] finalScore = new int[n];
 
-        average = total / score.length;
-        System.out.println("The class average score is " + average);
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalScore[i] = sc.nextInt();
         }
+
+        double passTotal = 0;
+        int passCount = 0;
+
+        double failTotal = 0;
+        int failCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (finalScore[i] > 70) {
+                passTotal += finalScore[i];
+                passCount++;
+            } else {
+                failTotal += finalScore[i];
+                failCount++;
+            }
+        }
+
+        double passAverage = passTotal / passCount;
+        double failAverage = failTotal / failCount;
+
+        System.out.println("The average score of students who passed is " + passAverage);
+        System.out.println("The average score of students who failed is " + failAverage);
     }
 }
