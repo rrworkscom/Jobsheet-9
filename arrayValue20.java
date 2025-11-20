d7cad4d9b9db9200351379f827bec60b38d2fbb8
rrworkscom <rrworks010@gmail.com>
2025-11-20 16:50:31 +0700

    Addition of mark score

--- arrayValue20.java
+++ arrayValue20.java
@@ -6,12 +6,16 @@ public class arrayValue20 {
 
         int[] finalScore = new int[10];
 
-        for (int i = 0; i < finalScore.length; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.print("Enter the final score " + i + ": ");
             finalScore[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Final score " + i + " is " + finalScore[i]);
+            if (finalScore[i] > 70) {
+                System.out.println("Student " + i + " Passed!");
+            } else {
+                System.out.println("Student " + i + " Failed!");
+            }
         }
     }
 }
